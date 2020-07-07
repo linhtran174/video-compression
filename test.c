cf6bc92c7f49f864073cbc6065e91f829713a2ee
Linh Tran <linhtt@nextsmarty.com>
2020-07-07 17:49:26 +0700

    draft

--- test.c
+++ test.c
@@ -9,21 +9,12 @@ typedef struct _img{
 } Image;
 
 typedef unsigned int u32;
+typedef int i32;
 typedef unsigned short u16;
+typedef short i16;
 typedef unsigned char u8;
+typedef short i8;
 
-// char max(char a, char b){
-//     return (a > b)?a:b;
-// }
-// char min(char a, char b){
-//     return (a < b)?a:b;
-// }
-// u32 max(u32 a, u32 b){
-//     return (a > b)?a:b;
-// }
-// u32 min(u32 a, u32 b){
-//     return (a < b)?a:b;
-// }
 
 double max(double a, double b, double c) {
    return ((a > b)? (a > c ? a : c) : (b > c ? b : c));
@@ -35,7 +26,8 @@ double min(double a, double b, double c) {
 int blockSize = 8;
 int w = 1920, h = 1080;
 int pixSize = 3;
-int frameSize, buffSize, lineSize;
+int frameSize, buffSize, lineSize, MBSize;
+int blockVOffset, blockHOffset;
 
 int absDiff(int a, int b){
     return (a > b)?(a-b):(b-a);
@@ -57,86 +49,102 @@ int MAD(u8* block1, u8* block2){
         }
     }
 
-    result /= (blockSize * blockSize * pixSize);
+    result /= MBSize;
     return result;
 }
 
+enum _frameMode {intra, segmenting, inter};
+enum _MBtype {background, mObj};
+
+void motionSearch(u8 *currentFrame, u8 *prevFrame, int x, int y, i8 *motionVector){
+    int stepSize = 2; 
+    int cost;
+    char mVector[2]; 
+
+    int MBOffset = (x * blockVOffset) + (y * blockHOffset);
+    u8 *currentMB = currentFrame + MBOffset;
+    int candidateCount = 0;
+
+    //same pos
+    cost = MAD(currentMB, prevFrame + MBOffset);
+    if(cost <= 10){
+        // check cost 
+        mVector[0] = 0;
+        mVector[1] = 0;
+    }
+    
+    //top-left
+    if(x >= stepSize && y >= stepSize){
+        candidateMBs[candidateCount] = 
+        prevFrame + x*(blockSize - stepSize)*lineSize + y*(blockSize - stepSize);
+
+        candidateCount++;
+    }
+    MAD(currentMB, currentMB);
+
+}
 
 int main(){
-    FILE *f = fopen("/home/linh/Videos/dota_1920x1080_48fps/dota2.rgb24", "r"),
-    *outputFile = fopen("/home/linh/Videos/dota_1920x1080_48fps/dota2.segmented", "w+");
+    FILE *f = fopen("/home/linhtt/Videos/dota_1920x1080_48fps/dota2.rgb24", "r"),
+    *outputFile = fopen("/home/linhtt/Videos/dota_1920x1080_48fps/dota2.segmented", "w+");
 
     frameSize =  w*h*pixSize;
     buffSize = frameSize * 48;
     lineSize = w*pixSize;
     u8 *_48frames = (u8 *)malloc(buffSize);
     
-
-    int MBSize = blockSize * blockSize * 3;
+    MBSize = blockSize * blockSize * 3;
     int numVBlock = h/blockSize;
     int numHBlock = w/blockSize;
 
     // Block vertical & horizontal offset
-    int blockVOffset = lineSize * blockSize;
-    int blockHOffset = blockSize * pixSize;
-    
+    blockVOffset = lineSize * blockSize;
+    blockHOffset = blockSize * pixSize;
+
     size_t read = 0;
     for (int j = 0; j < 10; j++){
         read = fread(_48frames, buffSize, 1, f);
-        if(read != buffSize) return;
+        if(read != 1) return;
 
         //four step search 
         // u8 *current_MB = (u8 *)malloc(MBSize);
         // u8 mX = 0, mY = 0;
         
-
         u8 *currentMB, *currentFrame, *prevFrame;
-        int candidateCount = 0;
         u8 **candidateMBs = (u8 **)malloc(10 * sizeof(u8 *));
         // int x1, y1, x2, y2;
 
-        int MBOffset;
-        int stepSize = 2; 
-        int cost;
-        
-        short mVector[2]; 
 
+        u8 fMode = intra;
         for(int frame = 1; frame < 48; frame++){
 
             currentFrame = _48frames + frame * frameSize;
             prevFrame = _48frames + (frame - 1) * frameSize;
 
-            //four corner
-            // candidateCount = 4; 
+            // Intra mode
+            if(fMode = intra){
+                fwrite(&fMode, 1, 1, outputFile);
+                fwrite(currentFrame, frameSize, 1, outputFile);
+                fMode = segmenting;
+                continue;
+            }
 
-            // //top left 
-            // currentMB
-            // candidateMBs = 
-            
-            
-            for(int c = 0; c < numVBlock; c++){
-                for(int d = 0; d < numHBlock; d++){
-                    MBOffset = (c * blockVOffset) + (d * blockHOffset);
-                    currentMB = currentFrame + MBOffset;
-                    candidateCount = 0;
+            if(fMode = segmenting){
+                fwrite(&fMode, 1, 1, outputFile);
 
-                    //same pos
-                    cost = MAD(currentMB, prevFrame + MBOffset);
-                    if(cost <= 10){
-                        // check cost 
+                i8 cMotionVector[2];
+                for(int c = 0; c < numVBlock; c++){
+                    for(int d = 0; d < numHBlock; d++){
+                        
+                        motionSearch(currentFrame, prevFrame, c, d, cMotionVector);
+                        
+
                     }
-                    
-                    //top-left
-                    if(c >= stepSize && d >= stepSize){
-                        candidateMBs[candidateCount] = 
-                        prevFrame + c*(blockSize - stepSize)*lineSize + d*(blockSize - stepSize);
-
-                        candidateCount++;
-                    }
-                    MAD(currentMB, currentMB);
-
                 }
             }
+            
+            
+            
             // _48frames[1*frameSize + ] 
         }
 
@@ -148,5 +156,4 @@ int main(){
 
     // nv12ToPPM(fopen("output.ppm", "w+"), i);
 }
-
 
